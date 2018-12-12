61e50111e51d35ebe027aff9b65845a42410b638
desarrollo.konecta <desarrollo.konecta@cunix.net>
2018-12-12 16:44:01 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+*
+* Primary app file for the api
+*
+*/
+
+// Dependencies
+
+var server = require('./lib/server');
+//var workers = require('./lib/workers');
+
+// Declare the app
+
+var app = {};
+
+// Init function
+app.init = function(){
+    //Start the server
+    server.init();
+
+    //Start the workers
+    //workers.init();
+};
+
+// Execute
+app.init();
+
+// Export the app;
+
+module.exports = app;
